9a8a18900866f8f445a9435552df5cb6a34bb062
mysterioushgt <salonigarg152@gmail.com>
2024-07-11 11:20:22 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import './App.css'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Header from './Component/layout/Header'
 import Home from './Component/Home'
 import Footer from './Component/layout/Footer'
-import {Routes , Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import ProductDetail from './Component/product/ProductDetail'
 import Login from './Component/user/Login'
 import Register from './Component/user/Register'
@@ -19,33 +21,56 @@ import CheckoutStep from './Component/cart/CheckoutStep'
 import Shipping from './Component/cart/Shipping'
 import ConfirmOrder from './Component/cart/ConfirmOrder'
 import Payment from './Component/payment/Payment'
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 
 function App() {
+
+  const [stripeApiKey, setStripeApiKey] = useState("");
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/stripeapiKey");
+    setStripeApiKey(data.stripeApiKey);
+  }
+  // console.log(stripeApiKey)
   const dispatch = useDispatch()
 
-  useEffect(() =>{
+
+  useEffect(() => {
     dispatch(loadUser())
+    getStripeApiKey()
   })
   return (
     <>
-    <Header/>
-    <Routes>
-        <Route path="/" element={<Home/> } />
-        <Route path="productDetail/:id" element={<ProductDetail/> } />
-        <Route path="/login" element={<Login/> } />
-        <Route path="/register" element={<Register/> } />
-        <Route path="/dashboard" element={<Dashboard/> } />
-        <Route path="/contact" element={<Contact/> } />
-        <Route path="/cart" element={<Cart/> } />
-        <Route path="/checkout" element={<Checkout/> } />
-        <Route path="/profile" element={<Profile/> } />
-        <Route path="/shipping" element={<Shipping/> } />
-        <Route path="/checkoutStep" element={<CheckoutStep/> } />
-        <Route path="/order/confirm" element={<ConfirmOrder/> } />
-        <Route path="/payment" element={<Payment/> } />
-        </Routes>
-    <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="productDetail/:id" element={<ProductDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/checkoutStep" element={<CheckoutStep />} />
+        <Route path="/order/confirm" element={<ConfirmOrder />} />
+
+        {
+          stripeApiKey && (
+            <Route
+              path="/payment"
+             element={
+               <Elements stripe={loadStripe(stripeApiKey)}>
+                 <Payment />
+               </Elements>
+             }
+            />
+          )
+        }
+      </Routes>
+      <Footer />
     </>
   )
 }
--- src/Component/payment/Payment.jsx
+++ src/Component/payment/Payment.jsx
@@ -1,9 +1,105 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch , useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useAlert } from 'react-alert';
+import axios from 'axios';
+import {
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import CheckoutStep from "../cart/CheckoutStep";
+import './Payment.css'
+import { useNavigate } from 'react-router-dom';
 
+const options = {
+  style: {
+    base: {
+      frontSize: "16px"
+    },
+    invalid: {
+      color: "#9e2146"
+    }
+  }
+}
 function Payment() {
+  const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+
+  const alert = useAlert();
+  const stripe = useStripe();
+  const elements = useElements();
+  const payBtn = useRef(null);
+  const navigate = useNavigate();
+  const { shippingInfo, cartItems } = useSelector((state) => state.cart);
+  const { user } = useSelector((state) => state.auth);
+  // const { error } = useSelector((state) => state.newOrder);
+
+  const order = {
+    shippingInfo,
+    orderItems: cartItems,
+    itemsPrice: orderInfo.itemsPrice,
+    taxPrice: orderInfo.taxPrice,
+    shippingPrice: orderInfo.shippingPrice  ,
+    totalPrice: orderInfo.totalPrice,
+};
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Payment</div>
+    <>
+      <CheckoutStep shipping confirmOrder payment />
+
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg">
+            <h1 className="mb-4">Card Info</h1>
+            <div className="form-group">
+              <label htmlFor="card_num_field">Card Number</label>
+              <CardNumberElement
+                type="text"
+                id="card_num_field"
+                className="form-control"
+                options={options}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="card_exp_field">Card Expiry</label>
+              <CardExpiryElement
+                type="text"
+                id="card_exp_field"
+                className="form-control"
+                options={options}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="card_cvc_field">Card CVC</label>
+              <CardCvcElement
+                type="text"
+                id="card_cvc_field"
+                className="form-control"
+                options={options}
+              />
+            </div>
+            <button
+              id="pay_btn"
+              type="submit"
+              onClick={submitHandler}
+              className="btn btn-block py-3"
+            >
+              Pay
+            </button>
+
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
